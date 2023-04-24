8c92067be2b499605f1436ded09934594b68968d
Mikulas Mickal <mikulas.mickal@seznam.cz>
2023-04-24 12:32:34 +0200

    fibo completed

--- fibo.py
+++ fibo.py
@@ -1,5 +1,20 @@
+def Fibonnaciho_posloupnost(cislo):
+    if cislo < 2:
+        if cislo == 0:
+            return 0
+        else:
+            return 1
+
+    return Fibonnaciho_posloupnost(cislo - 1) + Fibonnaciho_posloupnost(cislo - 2)
+
+
 
 def main():
+    vstup = int(input("Vstupní číslo:"))
+    sekvence = [Fibonnaciho_posloupnost(cislo=vstup) for vstup in range(vstup + 1)]
+
+    print(sekvence)
+
     pass
 
 
